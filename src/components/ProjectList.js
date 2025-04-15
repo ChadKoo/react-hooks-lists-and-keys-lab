f66fc83e471e6f0d09865f8bac90f550f6cf037f
Chadwick Koo <chadwickkoo@outlook.com>
2025-04-15 15:34:11 +0300

    Modified files

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,12 +3,20 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+
+ 
+  
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
-    </div>
+      {projects.map((projectItem)=>
+      <div id="project-list" key={projectItem.id}>
+        {projectItem.name}
+      </div>
+    )}
+      </div>
   );
 }
+
 
 export default ProjectList;
